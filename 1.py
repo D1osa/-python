3dce780a11c7bbe4c2124a93d4a3d075f3ef4df9
D1osa <86167521+D1osa@users.noreply.github.com>
2021-07-04 16:24:40 +0300

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,14 @@
+name = input('Введите имя ')
+print('Привет', name,'!')
+
+age = 29
+print(age, 'лет')
+
+a = 10
+print(a)
+b = a / 5
+print(b)
+
+c = int(input('введите число '))
+result = c + 100500
+print(result)
